1c798d5c047d9ccdb5e825ad99a7c45c93e79dec
pratimamaiti14 <pratima.maiti@nagarro.com>
2024-01-09 19:57:00 +0530

    troubleshooting

--- .github/workflows/processTestClass.sh
+++ .github/workflows/processTestClass.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-export Test_classes=""
-export Test_LevelValue=""
+
 # Define a parameterized function
 my_function() {
     # $1 is the first parameter, $2 is the second parameter, and so on
     prDescription="$1"
     if [ -z "$prDescription" ]; then
-        Testclasses=""
-        TestLevelValue="NoTestRun"
-        echo "Test classes are blank and Test level is $TestLevelValue"
-        Test_classes=$Testclasses
-        Test_LevelValue=$TestLevelValue
+        export Test_classes=""
+        export Test_LevelValue="NoTestRun"
+        echo "Test classes are blank and Test level is $Test_LevelValue"
     fi
 }
 
 # Example usage:
-prDescription="RunSpecifiedTests Class1,Class2,Class3"
+prDescription="$1"
 my_function "$prDescription"
+
+# Assign the values to the global variables
 echo "Result 1: $Test_classes"
 echo "Result 2: $Test_LevelValue"
