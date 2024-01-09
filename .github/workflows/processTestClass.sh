308c8595da14b1fc09345b297660c299a31c20f9
pratimamaiti14 <pratima.maiti@nagarro.com>
2024-01-09 19:26:47 +0530

    adding shell script to

--- .github/workflows/processTestClass.sh
+++ .github/workflows/processTestClass.sh
@@ -3,18 +3,13 @@
 # Define a parameterized function
 my_function() {
     # $1 is the first parameter, $2 is the second parameter, and so on
-    local parameter1=$1
-    local parameter2=$2
-
-    # Perform some operations based on the parameters
-    result=$((parameter1 + parameter2))
-
-    # Return the result
-    echo $result
+    prDescription="$1"
+    if [ -z "$description" ]; then
+        Testclasses=""
+        TestLevelValue="NoTestRun"
+        echo "Test classes are blank and Test level is $TestLevelValue"
+        echo "Testclasses=$Testclasses, TestLevelValue=$TestLevelValue"
 }
 
-# Call the function with arguments and store the result
-output=$(my_function "$1" "$2")
-
-# Print the result
-echo "$output"
+prDescription="RunSpecifiedTests Class1,Class2,Class3"
+processtestclass "$prDescription""
