021a3cb3707efeef0921257f3b32c58acef7ff78
pratimamaiti14 <pratima.maiti@nagarro.com>
2024-01-10 14:34:08 +0530

    troubleshooting

--- .github/workflows/processTestClass.sh
+++ .github/workflows/processTestClass.sh
@@ -20,7 +20,7 @@ my_function() {
         echo "$TestLevel"
     elif ! [[ "${PR_DESCRIPTION,,}" = "runlocaltests" || "${PR_DESCRIPTION,,}" = "runalltestsinorg" ]]; then
         TestLevelValue="${PR_DESCRIPTION%% *}"
-        TestClassesValue="${PR_DESCRIPTION#* }"
+        TestClassesValue="${PR_DESCRIPTION#*${TestLevelValue} }"
         echo "$TestClassesValue"
         if [ "${TestLevelValue,,}" = "runspecifiedtests" ]; then
             TestClasses=""
