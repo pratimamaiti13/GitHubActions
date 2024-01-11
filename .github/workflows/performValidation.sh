0f7d78f90de2394165ea2a3ef9574c5c52319040
pratimamaiti14 <pratima.maiti@nagarro.com>
2024-01-11 13:18:07 +0530

    troubleshooting

--- .github/workflows/performValidation.sh
+++ .github/workflows/performValidation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 perform_validation() {
     set -x
-    local TestLevel=$1
-    local TestClasses=$2
+    local TestLevel="$1"
+    local TestClasses="$2"
     if [ ${TestLevel} = "NoTestRun" ]; then
         sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --wait 20 --dry-run --json
     elif [ ${TestLevel} = "RunLocalTests" ] || [ ${TestLevel} = "RunAllTestsInOrg" ]; then
