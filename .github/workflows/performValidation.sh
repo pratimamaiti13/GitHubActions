afa0bf2996e2372210adc592de95a10ad160fa59
Pratima Maiti <121009549+pratimanagarro@users.noreply.github.com>
2024-01-29 14:42:49 +0530

    Create performValidation.sh

---
+++ .github/workflows/performValidation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+perform_validation() {
+    set -x
+    args=("$@")
+    local TestLevel=${args[0]}
+    local TestClasses=${args[1]}
+    echo ${args[1]}
+    if [ ${TestLevel} = "NoTestRun" ]; then
+        sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --wait 20 --dry-run --json
+    elif [ ${TestLevel} = "RunLocalTests" ] || [ ${TestLevel} = "RunAllTestsInOrg" ]; then
+        sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --wait 20 --dry-run --json
+    elif [ ${TestLevel} = "RunSpecifiedTests" ]; then
+        sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --tests $TestClasses --wait 20 --dry-run --json
+    else
+        echo "Test level and test class value are not correct."
+    fi
+}
+perform_validation "$@"
