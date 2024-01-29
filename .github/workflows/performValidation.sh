d926df8d87467a93200294ca0f9f199d27022664
pratimanagarro <pratima.maiti@nagarro.com>
2024-01-29 14:58:15 +0530

    removed troubleshooting step

--- .github/workflows/performValidation.sh
+++ .github/workflows/performValidation.sh
@@ -6,7 +6,7 @@ perform_validation() {
     local TestClasses=${args[1]}
     echo ${args[1]}
     if [ ${TestLevel} = "NoTestRun" ]; then
-        sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --wait 20 --dry-run --json
+        sf project deploy start --manifest Delta/package/package.xml --target-org ${{ env.SF_USERNAME }} --wait 20 --dry-run --json
     elif [ ${TestLevel} = "RunLocalTests" ] || [ ${TestLevel} = "RunAllTestsInOrg" ]; then
         sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --wait 20 --dry-run --json
     elif [ ${TestLevel} = "RunSpecifiedTests" ]; then
