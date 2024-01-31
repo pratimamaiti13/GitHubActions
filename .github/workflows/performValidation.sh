3e4e6abb76ad19b2fa457950ef0e070ca78a562f
pratimanagarro <pratima.maiti@nagarro.com>
2024-01-31 11:57:34 +0530

    modifying

--- .github/workflows/performValidation.sh
+++ .github/workflows/performValidation.sh
@@ -8,12 +8,13 @@ perform_validation() {
     echo "Number of lines in destructive is $TotalLineInDestructiveChanges"
     echo ${args[1]}
     if [ ${TotalLineInDestructiveChanges} > 4 ]; then
+        cd Delta/
         if [ ${TestLevel} = "NoTestRun" ]; then
-            sf project deploy start --manifest Delta/package/package.xml --pre-destructive-changes Delta/destructiveChanges/destructiveChanges.xml --target-org $SF_USERNAME --wait 20 --dry-run --json
+            sf project deploy start --manifest package/package.xml --pre-destructive-changes destructiveChanges/destructiveChanges.xml --target-org $SF_USERNAME --wait 20 --dry-run --json
         elif [ ${TestLevel} = "RunLocalTests" ] || [ ${TestLevel} = "RunAllTestsInOrg" ]; then
-            sf project deploy start --manifest Delta/package/package.xml --pre-destructive-changes Delta/destructiveChanges/destructiveChanges.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --wait 20 --dry-run --json
+            sf project deploy start --manifest package/package.xml --pre-destructive-changes destructiveChanges/destructiveChanges.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --wait 20 --dry-run --json
         elif [ ${TestLevel} = "RunSpecifiedTests" ]; then
-            sf project deploy start --manifest Delta/package/package.xml --pre-destructive-changes Delta/destructiveChanges/destructiveChanges.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --tests $TestClasses --wait 20 --dry-run --json
+            sf project deploy start --manifest package/package.xml --pre-destructive-changes destructiveChanges/destructiveChanges.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --tests $TestClasses --wait 20 --dry-run --json
         else
             echo "Test level and test class value are not correct."
         fi
