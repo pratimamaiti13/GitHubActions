172deab578c0516ef5b83459216deefc27f33df8
pratimanagarro <pratima.maiti@nagarro.com>
2024-02-02 17:03:42 +0530

    modifying shell scripting

--- .github/workflows/performValidation.sh
+++ .github/workflows/performValidation.sh
@@ -21,11 +21,11 @@ perform_validation() {
         fi
     else 
         if [ ${TestLevel} = "NoTestRun" ]; then
-            sf project deploy start --manifest Delta/package/package.xml --target-org $SF_USERNAME --wait 20 --dry-run --json
+            sf project deploy start --manifest package/package.xml --target-org $SF_USERNAME --wait 20 --dry-run --json
         elif [ ${TestLevel} = "RunLocalTests" ] || [ ${TestLevel} = "RunAllTestsInOrg" ]; then
-            sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --wait 20 --dry-run --json
+            sf project deploy start --manifest package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --wait 20 --dry-run --json
         elif [ ${TestLevel} = "RunSpecifiedTests" ]; then
-            sf project deploy start --manifest Delta/package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --tests $TestClasses --wait 20 --dry-run --json
+            sf project deploy start --manifest package/package.xml --target-org pratimamaiti@nagarro.com --test-level $TestLevel --tests $TestClasses --wait 20 --dry-run --json
         else
             echo "Test level and test class value are not correct."
         fi
