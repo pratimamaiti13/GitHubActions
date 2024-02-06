d9c655221bcddcac0b12fd5964d9c714ad0b7f80
Pratima Maiti <121009549+pratimanagarro@users.noreply.github.com>
2024-02-06 15:46:42 +0530

    Update patimaLWC.js

--- force-app/main/default/lwc/patimaLWC/patimaLWC.js
+++ force-app/main/default/lwc/patimaLWC/patimaLWC.js
@@ -2,8 +2,4 @@ import { LightningElement } from 'lwc';
 
 export default class PatimaLWC extends LightningElement {
     // Faulty performance: Synchronous heavy computation during rendering
-function isTruthy(x) {
-    debugger;
-    return Boolean(x);
-}
 }
